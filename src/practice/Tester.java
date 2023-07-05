4221f584fab8ae11dd5ac089051100c103879c33
Pranay7160 <hashtagpranay08@gmail.com>
2023-07-05 19:53:16 +0530

    made changes in Tester

--- src/practice/Tester.java
+++ src/practice/Tester.java
@@ -4,5 +4,10 @@ public class Tester {
 
 	public static void main(String []args) {
 		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		
 	}
 }
